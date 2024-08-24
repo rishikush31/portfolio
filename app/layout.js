e0e7bfac34fb8be45ab42127dde4b175484c9a35
Rishi <rishikush31@gmail.com>
2024-08-25 02:08:45 +0530

    o

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./Providers";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
